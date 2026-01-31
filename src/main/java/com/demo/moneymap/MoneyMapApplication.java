0a0c0ab424889fc99c3dc8303ed23a1ca7b4147c
SargamPuram <sargampuram3@gmail.com>
2026-01-31 19:06:53 +0530

    Initial project setup

---
+++ src/main/java/com/demo/moneymap/MoneyMapApplication.java
@@ -0,0 +1,13 @@
+package com.demo.MoneyMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMapApplication.class, args);
+	}
+
+}
